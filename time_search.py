b9061bcd615a73fc4f9e1f8d516c20012ba5a2c0
Dxd4 <46905365+Dxd4@users.noreply.github.com>
2022-10-06 19:47:40 +0000

    Update time_search.py

--- time_search.py
+++ time_search.py
@@ -1,11 +1,15 @@
 import re
 
 string = "Завтрак будет в 16:00 Футбольный счет 24:30"
-words = string.split(" ")
+def main(string):
+	words = string.split(" ")
 
-for word in words:
-	time = re.findall(r"[0-2][0-9]:[0-6][0-9]",word)
-	if len(time) < 1:
-		continue
-	if int(time[0].replace(":","")) < 2400:
-		print(time[0])
+	for word in words:
+		time = re.findall(r"[0-2][0-9]:[0-6][0-9]",word)
+		if len(time) < 1:
+			continue
+		if int(time[0].replace(":","")) < 2400:
+			print(time[0])
+
+if __name__ == "__main__":
+	main(string)
